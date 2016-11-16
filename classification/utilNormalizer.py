c5d90d9cd3b55c054f0e9aa829b180056fb12ae2
UgoOV <ugo.piqueras@openvalue.fr>
2016-11-16 14:16:41 +0100

    Redefining functions into class

--- classification/utilNormalizer.py
+++ classification/utilNormalizer.py
@@ -7,64 +7,92 @@ from nltk.stem.snowball import FrenchStemmer
 
 class Normalizer:
     """ Normalize Text """
-    def __init__(self, row):
-        self.row = row
 
-    list_stop_word_french=['alors','au','aucuns','aussi','autre','avant','avec','avoir','bon','car','ce','cela','ces','ceux','chaque',
-                           'ci','comme','comment','dans','des','du','dedans','dehors','depuis','devrait','doit','donc','dos','début',
-                           'elle','elles','en','encore','essai','est','et','eu','fait','faites','fois','font','hors','ici','il','ils',
-                           'je','juste','la','le','les','leur','là','ma','maintenant','mais','mes','mine','moins','mon','mot','même',
-                           'ni','nommés','notre','nous','ou','où','par','parce','pas','peut','peu','plupart','pour','pourquoi','quand',
-                           'que','quel','quelle','quelles','quels','qui','sa','sans','ses','seulement','si','sien','son','sont','sous',
-                           'soyez','sujet','sur','ta','tandis','tellement','tels','tes','ton','tous','tout','trop','très','tu','voient',
-                           'vont','votre','vous','vu','ça','étaient','état','étions','été','être', 'a']
+    def __init__(self):
+        self.list_stop_word_french = ['alors', 'au', 'aucuns', 'aussi', 'autre', 'avant', 'avec', 'avoir', 'bon', 'car',
+                                      'ce',
+                                      'cela', 'ces', 'ceux', 'chaque',
+                                      'ci', 'comme', 'comment', 'dans', 'des', 'du', 'dedans', 'dehors', 'depuis',
+                                      'devrait',
+                                      'doit', 'donc', 'dos', 'début',
+                                      'elle', 'elles', 'en', 'encore', 'essai', 'est', 'et', 'eu', 'fait', 'faites',
+                                      'fois',
+                                      'font', 'hors', 'ici', 'il', 'ils',
+                                      'je', 'juste', 'la', 'le', 'les', 'leur', 'là', 'ma', 'maintenant', 'mais', 'mes',
+                                      'mine',
+                                      'moins', 'mon', 'mot', 'même',
+                                      'ni', 'nommés', 'notre', 'nous', 'ou', 'où', 'par', 'parce', 'pas', 'peut', 'peu',
+                                      'plupart', 'pour', 'pourquoi', 'quand',
+                                      'que', 'quel', 'quelle', 'quelles', 'quels', 'qui', 'sa', 'sans', 'ses',
+                                      'seulement', 'si',
+                                      'sien', 'son', 'sont', 'sous',
+                                      'soyez', 'sujet', 'sur', 'ta', 'tandis', 'tellement', 'tels', 'tes', 'ton',
+                                      'tous', 'tout',
+                                      'trop', 'très', 'tu', 'voient',
+                                      'vont', 'votre', 'vous', 'vu', 'ça', 'étaient', 'état', 'étions', 'été', 'être',
+                                      'a']
 
-    # Suppress number
-    reg_numb = re.compile('[^\D]')
-    # Suppress punctuation
-    reg_ponct = re.compile('[^a-z 0-9ÀÁÂÃÄÅàáâãäåÒÓÔÕÖØòóôõöøÈÉÊËèéêëÇçÌÍÎÏìíîïÙÚÛÜùúûüÿÑñ²°Ø×ßŠ”�œ…]')
-    # Suppress apostrophe
-    reg_apos = re.compile('(l\')')
+        # Suppress number
+        self.reg_numb = re.compile('[^\D]')
+        # Suppress punctuation
+        self.reg_ponct = re.compile('[^a-z 0-9ÀÁÂÃÄÅàáâãäåÒÓÔÕÖØòóôõöøÈÉÊËèéêëÇçÌÍÎÏìíîïÙÚÛÜùúûüÿÑñ²°Ø×ßŠ”�œ…]')
+        # Suppress apostrophe
+        self.reg_apos = re.compile('( l\')|( d\')|( n\')|( m\')')
 
-    # Suppress stop words
-    french_stopwords_ini = stopwords.words('french')
-    french_stopwords_ini.extend(list_stop_word_french)
-    french_stopwords = set(french_stopwords_ini)
+        # Suppress stop words
+        self.french_stopwords_ini = stopwords.words('french')
+        self.french_stopwords_ini.extend(self.list_stop_word_french)
+        self.french_stopwords = set(self.french_stopwords_ini)
 
-    # Stemming of words
-    stemmer = FrenchStemmer()
+        # Stemming of words
+        self.stemmer = FrenchStemmer()
 
-    def to_lower(self, row):
+    def to_lower(self, str1):
         """ to lower case """
-        return row.lower()
+        return str1.lower()
         # print 'low_case : ', str1
 
-    def suppress_number(self):
+    def suppress_number(self, str1):
         """ Suppress number """
-        return reg_numb.sub('', str1)
+        return self.reg_numb.sub('', str1)
         # print 'only_letters : ', str1
 
-    # Suppress apostrophe
-    str1 = reg_apos.sub('', str1)
-    # print 'no apostrophe : ', str1
+    def suppress_apostrophe(self, str1):
+        """ Suppress apostrophe """
+        return self.reg_apos.sub(' ', str1)
+        # print 'no apostrophe : ', str1
 
-    # Suppress punctuation
-    str1 = reg_ponct.sub('', str1)
-    # print 'no_ponctuation : ', str1
+    def suppress_punctuation(self, str1):
+        """ Suppress punctuation """
+        return self.reg_ponct.sub('', str1)
+        # print 'no_ponctuation : ', str1
 
-    # Suppress stop words
-    str1 = [token for token in str1.split(' ') if token.lower() not in french_stopwords]
-    # print 'no_stop_words : ', str1
+    def suppress_stopword(self, str1):
+        """ Suppress stop words """
+        return [token for token in str1.split(' ') if token.lower() not in self.french_stopwords]
+        # print 'no_stop_words : ', str1
 
-    # Suppress accent
-    str1 = [word.encode('ascii', 'ignore') for word in str1]
-    # str1 = [unicodedata.normalize('NFD', unicode(word, 'utf-8')).encode('ascii', 'ignore') for word in str1]
-    # print 'no_accent : ', str1
+    def suppress_accent(self, str1):
+        """ Suppress accent """
+        # return [word.encode('ascii', 'ignore') for word in self.str1]
+        return [unicodedata.normalize('NFD', unicode(word, 'utf-8')).encode('ascii', 'ignore') for word in str1]
+        # print 'no_accent : ', str1
 
-    # Stemming of words
-    str1 = [stemmer.stem(word) for word in str1]
-    # print 'stemming : ', str1
+    def stemm_words(self, str1):
+        """ Stemming of words """
+        str2 = [word for word in str1 if word != '']
+        return [self.stemmer.stem(word) for word in str2]
+        # print 'stemming : ', str1
 
-    # Merging words
-    # print 'merge_list : ', ' '.join(str1)
-    return ' '.join(str1)
+    def end_to_end_normalize(self, str1):
+        """ Merging process """
+        str2 = self.stemm_words(
+            self.suppress_accent(
+                self.suppress_stopword(
+                    self.suppress_punctuation(
+                        self.suppress_apostrophe(
+                            self.suppress_number(
+                                self.to_lower(str1)
+                            ))))))
+        # print 'merge_list : ', ' '.join(str1)
+        return ' '.join(str2)
--- tests/test_normalizer.py
+++ tests/test_normalizer.py
@@ -1,9 +1,57 @@
+# -*- coding: utf-8 -*-
 from unittest import TestCase
 from utilNormalizer import Normalizer
 
 
 class TestNormalizer(TestCase):
-    def test_normaliz(self):
-        str1 = 'test des épées d\'aujourd\'hui'
+    def test_to_lower(self):
+        str1 = 'Test l\'épée manGeons d\'aujourd\'hui 3   '
+        n = Normalizer()
+        res = n.to_lower(str1)
+        self.assertEqual('test l\'épée mangeons d\'aujourd\'hui 3   ', res, "Problem with lower case")
 
-        self.fail()
+    def test_suppress_number(self):
+        str1 = 'test l\'épée mangeons d\'aujourd\'hui 3   '
+        n = Normalizer()
+        res = n.suppress_number(str1)
+        self.assertEqual('test l\'épée mangeons d\'aujourd\'hui    ', res, "Problem with suppress number")
+
+    def test_suppress_apostrophe(self):
+        str1 = 'test l\'épée mangeons d\'aujourd\'hui    '
+        n = Normalizer()
+        res = n.suppress_apostrophe(str1)
+        self.assertEqual('test épée mangeons aujourd\'hui    ', res, "Problem with suppress apostrophe")
+
+    def test_suppress_punctuation(self):
+        str1 = 'test épée mangeons aujourd\'hui    '
+        n = Normalizer()
+        res = n.suppress_punctuation(str1)
+        print str1 + '->' + res
+        self.assertEqual('test épée mangeons aujourdhui    ', res, "Problem with suppress punctuation")
+
+    def test_suppress_stopword(self):
+        str1 = 'a test épée mangeons aujourdhui    '
+        n = Normalizer()
+        res = n.suppress_stopword(str1)
+        self.assertEqual('test', res[0], "Problem with suppress stopwords")
+
+    def test_suppress_accent(self):
+        str1 = 'a test épée mangeons aujourdhui    '
+        n = Normalizer()
+        res_temp = n.suppress_stopword(str1)
+        res = n.suppress_accent(res_temp)
+        self.assertEqual('epee', res[1], "Problem with suppress accent")
+
+    def test_stemm_words(self):
+        str1 = 'a test épée mangeons aujourdhui    '
+        n = Normalizer()
+        res_temp = n.suppress_stopword(str1)
+        res_temp2 = n.suppress_accent(res_temp)
+        res = n.stemm_words(res_temp2)
+        self.assertEqual('epe', res[1], "Problem with stemming")
+
+    def test_end_to_end_normalize(self):
+        str1 = 'Test l\'épée manGeons d\'aujourd\'hui 3   '
+        n = Normalizer()
+        res = n.end_to_end_normalize(str1)
+        self.assertEqual('test epe mangeon aujourdhui', res, "Problem with end to end")
