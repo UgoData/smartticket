c0a9104669ba2486a0ecfeac0ee43fc485522ff6
Fanchouille <francois.valadier@openvalue.fr>
2016-11-18 17:18:48 +0100

    code reformatted

--- classification/tfidf_classification.py
+++ classification/tfidf_classification.py
@@ -8,38 +8,37 @@ from sklearn.model_selection import GridSearchCV
 from processTextData import ProcessText
 from loadAndCleanData import LoadCleanData
 import warnings
-warnings.filterwarnings("ignore")
 
+warnings.filterwarnings("ignore")
 
 p = ProcessText()
 l = LoadCleanData()
 
+
 class Classification:
     def __init__(self):
-        return None
+        self
 
-    def rfPipelineClassif(self, df,params_grid,num_folds=3):
+    def rfPipelineClassif(self, df, params_grid, num_folds=3):
         """ Classification pipeline with TFIDF and Random Forest avec input raw df"""
         X_df = p.clean_text_data(df)['merge_final']
         y = df['cat_purchease']
         vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5, min_df=0.005, ngram_range=(1, 2))
-        rf = RandomForestClassifier(n_estimators=10,random_state=123)
+        rf = RandomForestClassifier(n_estimators=10, random_state=123)
         pipe = Pipeline([('tfidf', vectorizer), ('rf', rf)])
-        model=GridSearchCV(pipe,param_grid=params_grid,n_jobs=1,cv=num_folds,verbose=1,refit=True)
+        model = GridSearchCV(pipe, param_grid=params_grid, n_jobs=1, cv=num_folds, verbose=1, refit=True)
 
-        return (model.fit(X_df, y),model.cv_results_)
+        return model.fit(X_df, y), model.cv_results_
 
 
 # Load all dataset
-df=l.load_and_concat()
+df = l.load_and_concat()
 print "Size of df :", df.shape
 
-params_grid={'tfidf__min_df':[0,0.0005,0.001],
-             'rf__min_samples_leaf': [1,3,5],
-             }
+params_grid = {'tfidf__min_df': [0, 0.0005, 0.001],
+               'rf__min_samples_leaf': [1, 3, 5],
+               }
 
-(model,results) = Classification().rfPipelineClassif(df,params_grid,num_folds=3)
+(model, results) = Classification().rfPipelineClassif(df, params_grid, num_folds=3)
 print results
-print model.predict(['pain maxi burger','HAR. VERT XF'])
-
-
+print model.predict(['pain maxi burger', 'HAR. VERT XF'])
